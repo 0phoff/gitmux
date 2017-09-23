98c22b4bae328a3d4f3d532f6c48f1fe8de40970
0phoff <0phoff@users.noreply.github.com>
2017-09-23 18:56:26 +0200

    It works!! :muscle:

--- gitmux_update.sh
+++ gitmux_update.sh
@@ -30,29 +30,29 @@ function main {
     if [ $? -eq 0 ]; then   # Git repo
         local git_fields=($("$current_dir/gitstatus.sh" "$dir" 2>/dev/null))
 
-        tmux setenv -g GITMUX_REPO              1 
-        tmux setenv -g GITMUX_BRANCH            "${git_fields[0]}"
-        tmux setenv -g GITMUX_REMOTE            "${git_fields[1]}"
-        tmux setenv -g GITMUX_COMMITS_AHEAD     "${git_fields[2]}"
-        tmux setenv -g GITMUX_COMMITS_BEHIND    "${git_fields[3]}"
-        tmux setenv -g GITMUX_STAGED            "${git_fields[4]}"
-        tmux setenv -g GITMUX_CONFLICTS         "${git_fields[5]}"
-        tmux setenv -g GITMUX_CHANGED           "${git_fields[6]}"
-        tmux setenv -g GITMUX_UNTRACKED         "${git_fields[7]}"
-        tmux setenv -g GITMUX_STASHED           "${git_fields[8]}"
-        tmux setenv -g GITMUX_CLEAN             "${git_fields[9]}"
+        tmux setenv GITMUX_REPO              1 
+        tmux setenv GITMUX_BRANCH            "${git_fields[0]}"
+        tmux setenv GITMUX_REMOTE            "${git_fields[1]}"
+        tmux setenv GITMUX_COMMITS_AHEAD     "${git_fields[2]}"
+        tmux setenv GITMUX_COMMITS_BEHIND    "${git_fields[3]}"
+        tmux setenv GITMUX_STAGED            "${git_fields[4]}"
+        tmux setenv GITMUX_CONFLICTS         "${git_fields[5]}"
+        tmux setenv GITMUX_CHANGED           "${git_fields[6]}"
+        tmux setenv GITMUX_UNTRACKED         "${git_fields[7]}"
+        tmux setenv GITMUX_STASHED           "${git_fields[8]}"
+        tmux setenv GITMUX_CLEAN             "${git_fields[9]}"
     else                    # No git repo
-        tmux setenv -g GITMUX_REPO 0
-        tmux setenv -g -u GITMUX_BRANCH
-        tmux setenv -g -u GITMUX_REMOTE
-        tmux setenv -g -u GITMUX_COMMITS_AHEAD
-        tmux setenv -g -u GITMUX_COMMITS_BEHIND
-        tmux setenv -g -u GITMUX_STAGED        
-        tmux setenv -g -u GITMUX_CONFLICTS     
-        tmux setenv -g -u GITMUX_CHANGED       
-        tmux setenv -g -u GITMUX_UNTRACKED     
-        tmux setenv -g -u GITMUX_STASHED       
-        tmux setenv -g -u GITMUX_CLEAN         
+        tmux setenv GITMUX_REPO 0
+        tmux setenv -u GITMUX_BRANCH
+        tmux setenv -u GITMUX_REMOTE
+        tmux setenv -u GITMUX_COMMITS_AHEAD
+        tmux setenv -u GITMUX_COMMITS_BEHIND
+        tmux setenv -u GITMUX_STAGED        
+        tmux setenv -u GITMUX_CONFLICTS     
+        tmux setenv -u GITMUX_CHANGED       
+        tmux setenv -u GITMUX_UNTRACKED     
+        tmux setenv -u GITMUX_STASHED       
+        tmux setenv -u GITMUX_CLEAN         
     fi
 
     tmux refresh-client -S
